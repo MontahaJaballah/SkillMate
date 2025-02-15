5dadb7bfe1860b329dea1ee19b8a53abbc1f58f8
jawher <touil.jawher@esprit.tn>
2025-02-15 13:37:15 +0100

    working show

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Define your routes here
+// Route to get all users
+router.get('/getAll', userController.showAllUsers);
+
+// Route to get a user by ID
+router.get('/getById/:id', userController.showUserById);
 
 module.exports = router;
