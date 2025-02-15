587b5bdf5e7ebd5bdf0d461971f3efc20df7c682
MontahaJaballah <montahabenjabalah@gmail.com>
2025-02-15 13:50:45 +0100

    deleteUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,17 @@
 const User = require('../models/User');
 
-// Your controller functions will go here
 
-module.exports = {};
+async function remove(req, res) {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        if (!deletedUser) {
+            return res.status(404).send({ error: "User not found" });
+        }
+        res.status(200).send({ message: "User deleted successfully" });
+    } catch (error) {
+        res.status(500).send({ error: error.toString() });
+    }
+}
+
+
+module.exports = {remove};
