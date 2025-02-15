9fe1333d0e347fac9409c548cd1d2eec1bcc0fb7
MontahaJaballah <montahabenjabalah@gmail.com>
2025-02-15 13:50:45 +0100

    deleteUser

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,10 +3,5 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.delete('/:id', userController.remove);
-// Route to get all users
-router.get('/getAll', userController.showAllUsers);
-
-// Route to get a user by ID
-router.get('/getById/:id', userController.showUserById);
 
 module.exports = router;
