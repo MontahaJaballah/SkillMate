94e3e1b641bda37705db78d2da2bb0eae0ac5c7a
MontahaJaballah <montahabenjabalah@gmail.com>
2025-02-15 15:07:51 +0100

    update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,7 +5,6 @@ const userController = require('../controllers/userController');
 // Define your routes here
 router.post("/adduser", userController.add);
 router.delete('/deleteuser/:id', userController.remove);
-
 router.post('/updateuser/:id', userController.update);
 
 module.exports = router;
