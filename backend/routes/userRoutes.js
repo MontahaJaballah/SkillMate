3ecea57eb4fa1b46c959ca939af4cfacf8c99eec
jawher <touil.jawher@esprit.tn>
2025-02-16 20:40:07 +0100

    login user backend and encrypted password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,5 +8,6 @@ router.delete('/deleteuser/:id', userController.remove);
 router.post('/updateuser/:id', userController.update);
 router.get("/allusers", userController.getAll);
 router.get("/getuser/:id", userController.getById);
-
+router.get("/searchuser/:username", userController.searchByUsername);
+router.post("/login", userController.login);
 module.exports = router;
