a52c097c83aa93554a96dfc8096ed01afd222539
jawher <touil.jawher@esprit.tn>
2025-02-16 20:40:07 +0100

    login user backend and encrypted password

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -57,5 +58,23 @@ const userSchema = new mongoose.Schema({
         ref: 'Course'
     }]
 });
+
+// **Pre-save middleware for password hashing**
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    try {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// **Method to compare passwords (for login)**
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
